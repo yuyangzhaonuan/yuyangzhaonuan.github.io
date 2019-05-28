ccf57d827f9a546cab62345059c7cedcee2fe27f
yuyang <2247295596@qq.com>
2019-05-28 16:12:21 +0800

    amendment css

--- collision.js
+++ collision.js
@@ -1,1 +1,6 @@
-"use strict";function sunMov(){sunX+=2*directX,sunY+=2*directY,dfg.style.top=sunY+"px",dfg.style.left=sunX+"px",(sunX+dfg.offsetWidth>=document.documentElement.clientWidth||sunX<=0)&&(directX=-directX),(sunY+dfg.offsetHeight>=document.documentElement.clientHeight||sunY<=0)&&(directY=-directY)}directX=1,directY=1,sunX=0,sunY=0,setInterval("sunMov()",25);
+"use strict";
+directX = 1, directY = 1, sunX = 0, sunY = 0
+function sunMov() {
+    sunX += 2 * directX, sunY += 2 * directY, dfg.style.top = sunY + "px", dfg.style.left = sunX + "px", (sunX + dfg.offsetWidth >= document.documentElement.clientWidth || sunX <= 0) && (directX = -directX), (sunY + dfg.offsetHeight >= document.documentElement.clientHeight || sunY <= 0) && (directY = -directY)
+}
+setInterval("sunMov()", 25);
