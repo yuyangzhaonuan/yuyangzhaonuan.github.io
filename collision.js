f03eb19bb36f9769abad261fe676764d0a1e7cef
yuyang <2247295596@qq.com>
2019-05-30 09:35:12 +0800

    screen shake

--- collision.js
+++ collision.js
@@ -1,1 +1,10 @@
-"use strict";function sunMov(){sunX+=2*directX,sunY+=2*directY,dfg.style.top=sunY+"px",dfg.style.left=sunX+"px",(sunX+dfg.offsetWidth>=document.documentElement.clientWidth||sunX<=0)&&(directX=-directX),(sunY+dfg.offsetHeight>=document.documentElement.clientHeight||sunY<=0)&&(directY=-directY)}var directX=1,directY=1,sunX=0,sunY=0;setInterval("sunMov()",25);
+"use strict";
+
+function sunMov() {
+    sunX += 2 * directX, sunY += 2 * directY, dfg.style.top = sunY + "px", dfg.style.left = sunX + "px", (sunX + dfg.offsetWidth >= document.documentElement.clientWidth-4 || sunX <= 0) && (directX = -directX), (sunY + dfg.offsetHeight >= document.documentElement.clientHeight-4 || sunY <= 0) && (directY = -directY)
+}
+var directX = 1,
+    directY = 1,
+    sunX = 0,
+    sunY = 0;
+setInterval("sunMov()", 25);
